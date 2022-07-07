4a51dc46e1bf09cb2afa0da8b2c0792663049946
Andrew Bocker <andrewjbocker@gmail.com>
2022-07-07 09:44:25 -0600

    Working phone code

---
+++ main.py
@@ -0,0 +1,110 @@
+import signal
+import sys
+import time
+import RPi.GPIO as GPIO
+import subprocess
+import threading
+from datetime import datetime
+
+# Setup GPIO pin for interrupts
+BUTTON_GPIO = 16
+
+# Finite State Machine for Phone
+# FSM states are:
+#   waiting - before any action
+#   playing - playing pre-recorded message
+#   recording - recording message via microphone
+
+# FSM Transitions are:
+#   waiting -> #play -> playing - GPIO interrupt, phone_down = False
+#   playing -> #stopPlayback ->  waiting - GPIO interrupt, phone_down = True
+#   playing -> #record -> recording - Playback ended, phone_down = False
+#   recording -> #stopRecording -> waiting - GPIO interrupt, phone_down = True
+
+class FSM:
+  def __init__(self):
+    self.state = 'waiting'
+    self.transitions = {
+      ('waiting', False): self.play,
+      ('playing', True): self.stopPlayback,
+      ('playing', False): self.record,
+      ('recording', True): self.stopRecording
+    }
+    self.p = None
+    self.r = None
+    self.message_count = 0
+
+  def handleGPIOEvent(self, phone_down):
+    if (self.state, phone_down) in self.transitions:
+      self.transitions[(self.state, phone_down)]()
+
+  def play(self):
+    print("Playing!")
+    self.state = 'playing'
+    # Start pre-recorded message via command
+    def start_via_thread():
+      time.sleep(0.25)
+      self.p = subprocess.Popen(['aplay', 'intro.wav'])
+      returncode = self.p.wait()
+      if returncode == 0:
+        # Playback finished completely
+        if not GPIO.input(BUTTON_GPIO):
+          self.record()
+        else:
+          self.wait()
+      else:
+        # Playback cancelled
+        self.wait()
+
+    threading.Thread(target=start_via_thread).start()
+
+  def stopPlayback(self):
+    if self.p:
+      self.p.kill()
+      self.p = None
+
+  def wait(self):
+    print("Waiting!")
+    self.state = 'waiting'
+    self.stopRecording()
+    self.stopPlayback()
+
+  def record(self):
+    print("Recording!")
+    self.state = 'recording'
+    filename = "recordings/" + datetime.now().strftime("%d-%b-%Y_%H-%M-%S") + "_recording.wav"
+    backup_spot = "/media/pi/4084-5B00/" + filename 
+    # Start recording via command
+    def start_via_thread():
+      self.r = subprocess.Popen(['arecord', '--device=hw:1,0', '--format', 'S16_LE', '-d', '180', '--rate', '44100', '-c1', filename])
+      self.r.wait()
+      subprocess.run(['cp', filename, backup_spot])
+      self.wait()
+
+    threading.Thread(target=start_via_thread).start()
+    self.message_count += 1
+
+  def stopRecording(self):
+    if self.r:
+      self.r.kill()
+      self.r = None
+
+
+# Create FSM object for tracking state
+fsm = FSM()
+
+def signal_handler(sig, frame):
+  GPIO.cleanup()
+  sys.exit(0)
+
+if __name__ == '__main__':
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+  # Boot up sound
+  subprocess.run(['aplay', 'sample-3s.wav'])
+
+  GPIO.add_event_detect(BUTTON_GPIO, GPIO.BOTH, callback=lambda _: fsm.handleGPIOEvent(GPIO.input(BUTTON_GPIO)), bouncetime=500)
+
+  signal.signal(signal.SIGINT, signal_handler)
+  signal.pause()
